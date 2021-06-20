49e217dc545e450b0e8af03096b99c836524f274
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-06-20 12:01:34 +0100

    Removing print statements.

--- __init__.py
+++ __init__.py
@@ -1,31 +1,22 @@
 from ._init import eax 
 
 
-
-
-print("IMPORTING GEOMETRY")
+#print("IMPORTING GEOMETRY")
 from .geometry.main import *
 from .geometry.CSG import Sphere
 
-print("IMPORTING MATERIAL")
+
+
+#print("IMPORTING MATERIAL")
 from .materials.materials import *
+
+
 from .sources import *
+
 
 from matplotlib.pyplot import *
 from numpy import *
 
 from .materials import database
-
-# # import numpy as np
-# # import pyximport
-# # pyximport.install(reload_support= True,
-# #                   inplace       = True,
-# #                   setup_args = {'include_dirs':np.get_include()})
-
-
-# print("____INIT_____")
-
-# print("from .materials.materials import eax")
-# from .materials.materials import eax
 
 
