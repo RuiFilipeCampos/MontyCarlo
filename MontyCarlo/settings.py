1f202c66e9a7953cd119d0c57f8aea5e40030701
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-06-30 14:21:17 +0100

    Update settings.py

--- MontyCarlo/settings.py
+++ MontyCarlo/settings.py
@@ -1,36 +1,23 @@
-print(">>>> SETTINGS.Py")
+print("SHOWING setting.py")
 
-
-#################################################################
-#################################################################
 __PATH__ = repr(__file__)[:-14][1:]
-#__PATH__ = r"C:\Users\Rui Campos\Dropbox\MontyCarlo\MontyCarlo" #<------- ONLY CHANGE THIS 
-#################################################################
-#################################################################
-
 
 print("_________________________________________________________________")
 print("INSTALL PATH: ", __PATH__)
 print("----")
 
-DEBUG = False
+DEBUG = False # runtime debug mode
 
 __photonCUTOFF__   = 10e3  
 __electronCUTOFF__ = 100e3
 
-SURFACE_THICKNESS = .001e-4  #500 um
 
 
-#__photonCUTOFF__ = __photonCUTOFF__*1e-6 / 0.511
+SURFACE_THICKNESS = .001e-4  #500 um --- this has no meaning, keeping it in case some older module is calling it
 
 
-Wcc = 10 #eV
+Wcc = 10 #eV < --- no meaning
 
-
-
-
-print(f"SURFACE_THICKNESS: {SURFACE_THICKNESS}")
-print("---------------")
 
 
 print(f"DEBUG: {DEBUG}")
@@ -42,20 +29,14 @@ print("----")
 print(f"ELECTRON CUT OFF: {__electronCUTOFF__} MeV")
 print("---------------------------------------------")
 
-print(f"Wcc: {Wcc} eV")
-print("---------------")
-
 print("");print("");
-
-
-
 
 
 
 Wcc = Wcc*1e-6 
 
 from pathlib import Path
+
 __montecarlo__ = Path(__PATH__)
 
-
-print("BRANCH: SETTING UP MIX MAX")
+print("BRANCH: default1 - pre-alpha")
