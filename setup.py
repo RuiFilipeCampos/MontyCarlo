f26e1c81cf6efe65f3cd9eeec3c08e5a0180130d
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-06-30 14:15:18 +0100

    Update setup.py
    Code is now cleaner.

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 27 12:41:36 2020
 
-@author: Rui Campos
-"""
-
-# from setuptools import setup
+import os
 from Cython.Build import cythonize
-
-# setup(
-#     ext_modules = cythonize("tools/*.pyx")
-# )
-
 
 try:
     from setuptools import setup, find_packages
@@ -24,79 +14,55 @@ from Cython.Distutils import build_ext
 import numpy as np
 
 
+args = ["-O2", "-fp:fast"] #MSVC ARGUMENTS
 
-
-
-
-args = ["-O2", "-fp:fast"]
-ext_modules = [
-Extension("tools.*",              ["MontyCarlo\\tools\\*.pyx"],               extra_compile_args = args),
-Extension("particles.*",          ["MontyCarlo\\particles\\*.pyx"],           extra_compile_args = args),
-Extension("*",                    ["MontyCarlo\\*.pyx"],                      extra_compile_args = args), 
-Extension("geometry.*",           ["MontyCarlo\\geometry\\*.pyx"],            extra_compile_args = args),
-Extension("materials.electron.*", ["MontyCarlo\\materials\\electron\\*.pyx"], extra_compile_args = args),
-Extension("materials.positron.*", ["MontyCarlo\\materials\\positron\\*.pyx"], extra_compile_args = args),
-Extension("materials.*",          ["MontyCarlo\\materials\\*.pyx"],           extra_compile_args = args),
-Extension("materials.photon.*",   ["MontyCarlo\\materials\\photon\\*.pyx"],   extra_compile_args = args),
-Extension("_random.*",             ["MontyCarlo\\_random\\*.pyx"],              extra_compile_args = args)
-]
-#-ffast-math
-
-#,Extension("geometry.CGS*", ["geometry\\CGS\\*.pyx"])]
-
-
-
-#ext_modules = [Extension("my_code_cython",["my_code_cython.pyx"]),
-#               Extension("another_code_cython",["another_code_cython.pyx"])]
-
-import os
-
+ext_modules = [ Extension("tools.*",               ["MontyCarlo\\tools\\*.pyx"],                extra_compile_args = args),
+                Extension("particles.*",           ["MontyCarlo\\particles\\*.pyx"],            extra_compile_args = args),
+                Extension("*",                     ["MontyCarlo\\*.pyx"],                       extra_compile_args = args), 
+                Extension("geometry.*",            ["MontyCarlo\\geometry\\*.pyx"],             extra_compile_args = args),
+                Extension("materials.electron.*",  ["MontyCarlo\\materials\\electron\\*.pyx"],  extra_compile_args = args),
+                Extension("materials.positron.*",  ["MontyCarlo\\materials\\positron\\*.pyx"],  extra_compile_args = args),
+                Extension("materials.*",           ["MontyCarlo\\materials\\*.pyx"],            extra_compile_args = args),
+                Extension("materials.photon.*",    ["MontyCarlo\\materials\\photon\\*.pyx"],    extra_compile_args = args),
+                Extension("_random.*",             ["MontyCarlo\\_random\\*.pyx"],              extra_compile_args = args)
+             ]
 
 # COMPILER OPTIONS
 import Cython.Compiler.Options
-
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
-#os.environ['CFLAGS'] = '-O3 -Wall -std=c++11 -I"some/custom/paths"'
 setup(
     name = "MontyCarlo", version = "0.0.35", author = "Rui Filipe de Sousa Campos",
     description = "A fast general purpose monte carlo particle simulator (photons, electrons and positrons). Written in Cython, Python and C++.",
-    long_description = long_description, long_description_content_type="text/markdown",     url="https://github.com/RuiFilipeCampos/MontyCarlo",
+    long_description = long_description, long_description_content_type="text/markdown", url="https://github.com/RuiFilipeCampos/MontyCarlo",
     setup_requires=['setuptools_scm'],
-    install_requires=['requests', 
-                      'gdown', 
-                      'numpy', 
-                      'scipy', 
-                      'matplotlib', 
-                      'pyvista', 
-                      'numba', 
-                      'bs4', 
-                      'pandas', 
-                      'plotly', 
+    install_requires=['requests',     # for downloading databases
+                      'gdown',        # for downloading databases
+                      'numpy',        # for data processing and C-API
+                      'scipy',        # for data processing
+                      'matplotlib',   # for data visualization
+                      'pyvista',      # for geometry debugging and data visualization
+                      'numba',        # for JIT compilation, will be deprecated in the future
+                      'bs4',          # for some other dependency
+                      'pandas',       # for data processing, data reading and for htmlcreator
+                      'plotly',       # for htmlcreator
                       'scikit-image',
                       'Jinja2', 
                       'pyunpack',
                       'patool'],
-    include_package_data=True,
-    packages=find_packages(),
-    cmdclass = {'build_ext': build_ext},
-    include_dirs = [".", np.get_include(), "_random"], #, "MontyCarlo\\materials"
+    include_package_data = True,
+    packages             = find_packages(),
+    cmdclass             = {'build_ext': build_ext},
+    include_dirs         = [".", np.get_include(), "_random"],
     ext_modules = cythonize(ext_modules, 
-    						annotate=False,
-                            compiler_directives={'profile': False, 'language_level' : "3"})) 
-
-
-
-
-
-
-#	print("Downloading positron elastic data...")
-#	#from .materials.positron import download_database
-
-
+    						annotate = False,                               # this is getting overriden locally ._.
+                            compiler_directives = {'profile': False,        # this is also getting overriden locally ._.
+                                                   'language_level' : "3"}
+                           )
+)  
 
 
 
