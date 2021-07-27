6cd1dd840ee4aaa102f5347b3f74618c4810dfe2
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-07-27 15:30:21 +0100

    Update download.py

--- dev/download_elastic/download.py
+++ dev/download_elastic/download.py
@@ -1,4 +1,24 @@
+__doc__ = """Download elastic scattering database from `https://ruifilipecampos.github.io/MontyCarlo/`.
+"""
+__author__ = "Rui Campos"
+
+
 import requests
 import os
 
+"""
+from ...settings import __montecarlo__
 
+PATH = __montecarlo__/'materials'/'electron'/'elastic'
+elastic_folder = str(PATH)
+"""
+
+os.mkdir(elastic_folder)
+
+url = r"https://ruifilipecampos.github.io/MontyCarlo/elastic/electron"
+top_level_files = ["HEeax.npy", "LEeax.npy", "muGRID.npy"]
+
+for filename in top_level_files:
+  file = requests.get(url + filename)
+  print(file)
+  
