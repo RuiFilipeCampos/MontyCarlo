228ec32e20d0787eb61566a49ec97b359028492c
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-07-27 14:38:40 +0100

    EADL download from github

--- MontyCarlo/materials/EADL/download_database.py
+++ MontyCarlo/materials/EADL/download_database.py
@@ -4,14 +4,15 @@ from ...settings import __montecarlo__
 
 PATH = __montecarlo__/'materials'/'EADL'
 
-
-for N in range(1, 10):
-    url = "https://www-nds.iaea.org/epics/ENDL2017/EADL.ELEMENTS/ZA00" + str(N) + "000"
+for N in range(1, 101):
+    #url = "https://www-nds.iaea.org/epics/ENDL2017/EADL.ELEMENTS/ZA00" + str(N) + "000"
+    url = "https://ruifilipecampos.github.io/MontyCarlo/EADL/" + str(N) + ".txt"
     file = requests.get(url)
     filename = str(N) + ".txt"
     open(str(PATH/filename), 'wb').write(file.content)
 
 
+"""
 for N in range(10, 100):
     url = "https://www-nds.iaea.org/epics/ENDL2017/EADL.ELEMENTS/ZA0" + str(N) + "000"
     file = requests.get(url)
@@ -22,3 +23,4 @@ url = "https://www-nds.iaea.org/epics/ENDL2017/EADL.ELEMENTS/ZA100000"
 file = requests.get(url)
 filename = "100.txt"
 open(str(PATH/filename), 'wb').write(file.content)
+"""
