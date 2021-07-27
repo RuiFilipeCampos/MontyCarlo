579b117adac0430aef745edd5eb552b2bdccde3d
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-07-27 15:24:04 +0100

    Update download_database.py

--- MontyCarlo/materials/electron/download_database.py
+++ MontyCarlo/materials/electron/download_database.py
@@ -1,9 +1,22 @@
 import requests
-
+import os
 from ...settings import __montecarlo__
 
 PATH = __montecarlo__/'materials'/'electron'/'elastic'
+elastic_folder = str(PATH)
 
+
+os.mkdir(elastic_folder)
+
+url = r"https://ruifilipecampos.github.io/MontyCarlo/elastic/electron"
+top_level_files = ["HEeax.npy", "LEeax.npy", "muGRID.npy"]
+
+for filename in top_level_files:
+  file = requests.get(url + filename)
+  
+
+
+"""
 import gdown
 
 url = 'https://drive.google.com/uc?id=1wod967aO8K90AtW9nvgtL8irRr0Go9iE'
@@ -13,5 +26,5 @@ gdown.download(url, output, quiet=False)
 from pyunpack import Archive
 
 Archive(output).extractall(str(__montecarlo__/'materials'/'electron'))
-
+"""
 
