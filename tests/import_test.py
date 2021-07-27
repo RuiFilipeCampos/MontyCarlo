a53e10e55c1cb5126c710adf87c850a3bf8cc6a0
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-07-27 14:32:48 +0100

    Create import_test.py

---
+++ tests/import_test.py
@@ -0,0 +1,15 @@
+__doc__ = """
+"""
+
+import sys
+module_arguments = sys.argv
+
+if module_arguments[0] == "from_automated_build":
+  path_to_myco = module_arguments[1]
+  
+  
+elif module_arguments[0] == ""
+
+
+
+import MontyCarlo as myco
