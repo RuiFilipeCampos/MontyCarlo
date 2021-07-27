48b35dcac20ceb6df12ac7561d4d79183e1e44d9
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-07-27 15:48:39 +0100

    fixing indentation

--- dev/download_elastic/download.py
+++ dev/download_elastic/download.py
@@ -26,22 +26,20 @@ url = r"https://ruifilipecampos.github.io/MontyCarlo/elastic/electron/"
 top_level_files = ["HEeax.npy", "LEeax.npy", "muGRID.npy"]
 
 for filename in top_level_files:
-  with requests.get(url + filename) as file:
-	  with open(str(__folder__/'elastic'/filename), 'wb') as local_file:
-	  	local_file.write(file.content)
+	with requests.get(url + filename) as file:
+		with open(str(__folder__/'elastic'/filename), 'wb') as local_file:
+			local_file.write(file.content)
 
 
 element_level_files = ["DCS.npy", "HEtransportTCS.npy", "LEtransportTCS.npy"]
 
 for i in range(1, 100):
 	element_folder = str(__folder__/'elastic'/str(i))
-
 	os.mkdir(element_folder)
-
 	for filename in top_level_files:
-	  with requests.get(url + filename) as file:
-		  with open(str(__folder__/'elastic'/str(i)/filename), 'wb') as local_file:
-		  	local_file.write(file.content)
+		with requests.get(url + filename) as file:
+			with open(str(__folder__/'elastic'/str(i)/filename), 'wb') as local_file:
+				local_file.write(file.content)
 
 
 
