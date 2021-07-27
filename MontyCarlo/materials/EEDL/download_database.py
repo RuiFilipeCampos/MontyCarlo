d7fbe0843381199ff2525f5957f0e6c3b0203df1
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-07-27 15:05:11 +0100

    download db from github

--- MontyCarlo/materials/EEDL/download_database.py
+++ MontyCarlo/materials/EEDL/download_database.py
@@ -1,21 +1,25 @@
 import requests
 from ...settings import __montecarlo__
 
-PATH = __montecarlo__/'materials'/'EEDL'
-for N in range(1, 10):
-    url = "https://www-nds.iaea.org/epics/ENDL2017/EEDL.ELEMENTS/ZA00" + str(N) + "000"
+PATH = __montecarlo__/'materials'/'EADL'
+
+for N in range(1, 101):
+    #url = "https://www-nds.iaea.org/epics/ENDL2017/EADL.ELEMENTS/ZA00" + str(N) + "000"
+    url = "https://ruifilipecampos.github.io/MontyCarlo/EEDL/" + str(N) + ".txt"
     file = requests.get(url)
     filename = str(N) + ".txt"
     open(str(PATH/filename), 'wb').write(file.content)
 
 
+"""
 for N in range(10, 100):
-    url = "https://www-nds.iaea.org/epics/ENDL2017/EEDL.ELEMENTS/ZA0" + str(N) + "000"
+    url = "https://www-nds.iaea.org/epics/ENDL2017/EADL.ELEMENTS/ZA0" + str(N) + "000"
     file = requests.get(url)
     filename = str(N) + ".txt"
     open(str(PATH/filename), 'wb').write(file.content)
 
-url = "https://www-nds.iaea.org/epics/ENDL2017/EEDL.ELEMENTS/ZA100000"
+url = "https://www-nds.iaea.org/epics/ENDL2017/EADL.ELEMENTS/ZA100000"
 file = requests.get(url)
 filename = "100.txt"
 open(str(PATH/filename), 'wb').write(file.content)
+"""
