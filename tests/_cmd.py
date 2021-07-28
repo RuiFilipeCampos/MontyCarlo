7cbfc134fa933aab89622b868aadbfc5cc4f40a5
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-07-28 06:33:46 +0100

    update _cmd.py

--- tests/_cmd.py
+++ tests/_cmd.py
@@ -10,3 +10,26 @@ This is necessary since there are several possible scenarios when unit testing M
 __author__ = "Rui Campos"
 
 
+import sys
+from pathlib import Path
+
+if len(sys.argv) == 1:
+	raise RuntimeError("Missing arguments.")
+
+arg1 = sys.argv[1]
+
+if arg1 == "--pip_installed" or arg1 == "-p":
+	pass
+
+elif arg1 == "--built_inplace" or arg1 == "-b":
+	__path__ = Path(sys.argv[0])
+	__folder__ = __path__.parent
+	sys.path.append(str(__folder__.parent))
+	print(f"The path '{sys.path[-1]}' has been appended to `sys.path`. If the path looks weird, you probably ran the script as a python file. Please run it as a module:")
+	print("`python -m unit_test --built_inplace`")
+
+else:
+	raise RuntimeError("No arguments at all.")
+
+
+
