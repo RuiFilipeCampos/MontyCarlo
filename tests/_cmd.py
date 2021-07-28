0e9c0820c5260cdbff4c0c9a475c2ad3fc8fe0a2
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-07-28 06:12:55 +0100

    Create _cmd.py

---
+++ tests/_cmd.py
@@ -0,0 +1,12 @@
+__doc__ = """Handling command line arguments for all unit testing modules. 
+
+This is necessary since there are several possible scenarios when unit testing Monty Carlo:
+
+    (1) MyCo is installed through pip
+    (2) MyCo has been built inplace at `/MontyCarlo`
+    (3) ...
+"""
+
+__author__ = "Rui Campos"
+
+
