6ca951f6ca44c752682d3a561ed2ccfc67790eeb
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-07-28 09:22:22 +0100

    Update setup_version.py

--- setup_version.py
+++ setup_version.py
@@ -3,7 +3,7 @@ __doc__ = """Setting up a new version.
 
 __author__ = "Rui Campos"
 
-version = 0.0.41 # to be imported by setup*.py scripts
+version = "0.0.41" # to be imported by setup*.py scripts
 
 
 ### to be added: open files and write version
