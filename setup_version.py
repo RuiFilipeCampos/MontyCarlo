3ab35fa704f677d477d571d217bd5ea053f5ddeb
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-07-28 10:10:51 +0100

    Update setup_version.py

--- setup_version.py
+++ setup_version.py
@@ -7,7 +7,7 @@ version = "0.0.41" # to be imported by setup*.py scripts
 
 
 with open("setup.cfg", "w") as setup_cfg:
-  text = setup_cfg.read_lines()
+  text = setup_cfg.readlines()
   print(text)
 
 ### to be added: open files and write version
