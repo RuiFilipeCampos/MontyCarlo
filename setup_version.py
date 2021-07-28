91f37edfb990915934d4dcf6ba23d8201459c8a1
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-07-28 10:11:27 +0100

    Update setup_version.py

--- setup_version.py
+++ setup_version.py
@@ -6,8 +6,13 @@ __author__ = "Rui Campos"
 version = "0.0.41" # to be imported by setup*.py scripts
 
 
-with open("setup.cfg", "w") as setup_cfg:
+with open("setup.cfg", "r") as setup_cfg:
   text = setup_cfg.readlines()
   print(text)
+
+  
+with open("setup.cfg", "w") as setup_cfg:
+  pass
+
 
 ### to be added: open files and write version
