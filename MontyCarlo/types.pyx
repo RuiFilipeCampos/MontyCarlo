063048e676f35e7ef8aa95263dfd39be9d4f6841
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-07-31 18:46:30 +0100

    Added MAP.

--- MontyCarlo/types.pyx
+++ MontyCarlo/types.pyx
@@ -14,3 +14,32 @@ nan = np.nan
 
 cdef struct double3:
     double x, y, z
+    
+    
+class MAP(dict):
+    """A dictionary with the following syntax sugar:
+    
+    > map = MAP()
+    > map.x = 1
+    
+    is equivelent to:
+    
+    > map = MAP()
+    > map['x'] = 1
+    
+    """
+    
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError:
+            raise AttributeError
