deaa2812867aeb9065090cb386ebe3ae97452904
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-08-01 19:11:59 +0100

    Added some output when downloading db.

--- MontyCarlo/materials/electron/download_database.py
+++ MontyCarlo/materials/electron/download_database.py
@@ -26,7 +26,10 @@ url = r"https://ruifilipecampos.github.io/MontyCarlo/elastic/electron/"
 top_level_files = ["HEeax.npy", "LEeax.npy", "muGRID.npy"]
 
 for filename in top_level_files:
-	with requests.get(url + filename) as file:
+	url_file = url + filename
+	print("Downloading from: " url_file)
+
+	with requests.get(url_file) as file:
 		with open(str(__folder__/'elastic'/filename), 'wb') as local_file:
 			local_file.write(file.content)
 
@@ -37,6 +40,8 @@ for i in range(1, 100):
 	element_folder = str(__folder__/'elastic'/str(i))
 	os.mkdir(element_folder)
 	for filename in element_level_files:
+		url_file = url + filename
+		print("Downloading from: " url_file)
 		with requests.get(url + filename) as file:
 			with open(str(__folder__/'elastic'/str(i)/filename), 'wb') as local_file:
 				local_file.write(file.content)
