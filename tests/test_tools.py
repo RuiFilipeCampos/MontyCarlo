b57bbef8a4e3b220d7f38411b000fc37b5843ed9
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-08-01 13:58:11 +0100

    Added test o testsuite.

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -78,5 +78,29 @@ class Test_remove_duplicates(ut.TestCase):
                        f"Should be {ground_truth.Y}"
                        )
 
+    def test2(self):
+
+        input_val.X = np.array([ 1, 1, 1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 1000, 1000, 5000])
+        input_val.Y = input_val.X**2
+
+        ground_truth.X = np.array([1, 2, 3, 4, 5, 6, 7, 1000, 5000])
+        ground_truth.Y = ground_truth.X**2
+
+        output_val.X, output_val.Y = remove_duplicates(input_val.X, input_val.Y)
+
+        self.assertEqual(
+                       list(output_val.X), 
+                       list(ground_truth.X), 
+                       f"Should be {ground_truth.X}"
+                       )
+
+        self.assertEqual(
+                       list(output_val.Y), 
+                       list(ground_truth.Y),
+                       f"Should be {ground_truth.Y}"
+                       )
+
+
+
 if __name__ == '__main__':
     ut.main()
