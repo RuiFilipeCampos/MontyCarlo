a1e726ce50587ab6f78c16f09d02a6ccbb886588
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-08-03 10:02:02 +0100

    Introducing py_state. Moving STATE to .types

--- MontyCarlo/types.pxd
+++ MontyCarlo/types.pxd
@@ -1,4 +1,19 @@
+from .external.mixmax_interface cimport mixmax_engine
+
 cdef double nan
 
 cdef struct double3:
     double x, y, z
+
+cdef struct STATE:
+	mixmax_engine* genPTR
+	void *current_region
+	double3 pos
+	double3 dire
+	double3 axis
+	double E
+	double L 
+	double last_displacement
+
+cdef class py_state:
+	cdef STATE
--- MontyCarlo/types.pyx
+++ MontyCarlo/types.pyx
@@ -43,3 +43,42 @@ class MAP(dict):
             del self[key]
         except KeyError:
             raise AttributeError
+
+
+
+cdef class py_state:
+    """The python analog of `STATE`:
+
+    ```
+    cdef struct STATE:
+        mixmax_engine* genPTR
+        void *current_region
+        double3 pos
+        double3 dire
+        double3 axis
+        double E
+        double L 
+        double last_displacement
+    ```
+
+    """
+
+    def to_cython(self):
+
+        self.state.pos.x = self.pos[0]
+        self.state.pos.y = self.pos[1]
+        self.state.pos.z = self.pos[2]
+
+        self.state.dir.x = self.dir[0]
+        self.state.dir.y = self.dir[1]
+        self.state.dir.z = self.dir[2]
+
+        self.state.axis.x = self.axis[0]
+        self.state.axis.y = self.axis[1]
+        self.state.axis.z = self.axis[2]
+
+        self.state.E = self.E
+        self.state.L = self.L
+        self.state.last_displacement = self.last_displacement
+
+        return self.state
