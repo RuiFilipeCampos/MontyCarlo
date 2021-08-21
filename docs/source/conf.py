0c4b0d27254020d5101b1a047c96a27c1420a3a9
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-08-21 11:49:44 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,21 @@ release = '0.1a0.dev2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+
+
+html_theme = "sphinx_rtd_theme"
+
 extensions = [
 ]
+
+import sphinx_rtd_theme
+
+extensions = [
+    ...
+    'sphinx_rtd_theme',
+]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
