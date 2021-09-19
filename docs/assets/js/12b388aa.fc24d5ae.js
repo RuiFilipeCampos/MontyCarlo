edb38cc55eb668232218da2ae987c1a194ed697f
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-09-19 11:01:03 +0100

    docs:pasted new build

---
+++ docs/assets/js/12b388aa.fc24d5ae.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoc_dev=self.webpackChunkdoc_dev||[]).push([[98],{1528:function(e){e.exports=JSON.parse('{"permalink":"/MontyCarlo/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ docs/assets/js/55960ee5.4bb2abad.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoc_dev=self.webpackChunkdoc_dev||[]).push([[121],{8070:function(e){e.exports=[]}}]);
