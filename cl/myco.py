087f2f82ca27f7c2b0d92806d06f583e08c70142
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-10-17 10:12:48 +0100

    fixing shell color

--- cl/myco.py
+++ cl/myco.py
@@ -1,33 +1,105 @@
-import argparse
+BOILER_PLATE = """
+
+
+# Imports will be cleaner eventually...
+from MontyCarlo import *
+from MontyCarlo.sources import *
+from MontyCarlo.geometry.CSG import *
+
+
+# Define your materials. They are compiled once per project, so don't worry if it takes a while
+# the first time you run the script, the second time `Mat` will just load from cache.
+
+water = Mat({1:2, 8:1}, 1, name = "Water")
+
+air = Mat({6:1.50187000E-04,
+           7:7.84430000E-01,
+           8:2.10748000E-01,
+           18:4.67111000E-03}, 
+           1.20479e-03,
+           C1 = .2, 
+           C2 = .2,
+           name = "AirDryNearSeaLevel")
+
+gold = Mat({79:1}, 1.93200000E+01, name = "Gold")
+
+
+# Define geometry
+# The indentation tells you exactly how the BVH is being constructed.
+# The `.configure` method will be replaced with a smarter system.
+with InfiniteVolume() as outer:
+    outer.configure("OUTER", render = False)
+    outer.fill(gold) 
+    
+    with Sphere(100) as outer_sphere:
+        outer_sphere in outer
+        outer_sphere.configure("outer_sphere", render = True)
+        outer_sphere.fill(air)
+
+        with Sphere(50) as inner_sphere:
+            inner_sphere in outer_sphere
+            inner_sphere.configure("inner_sphere", render = True)
+            inner_sphere.fill(water)
+
+
+
+photon_beam = Beam(
+                   "photon",       # kind of particle 
+                   inner_sphere,   # initial volume
+                   E = 10e6,       # initial eneryg in eV
+                   N = 1_000,     # number of particles in the source, careful with this number, might break your run and fill your ram
+                   pos = (0, 0, 0) # initial position 
+                  ) 
+
+
+
+# let Plotter handle the run
+plotter = Plotter(photon_beam)
+
+
+# then ask it for a fig
+fig = plotter.new_plot()
+
+# use this method to draw the geometry onto the figure (this will be better)
+plotter.add_geometry(fig, outer)
+
+
+fig.show()
+"""
+
+
+
+
+from os import error
 import sys
 from pathlib import Path
 
-print(Path(sys.argv[0]))
 import cmd, sys
-from turtle import *
-import colorama
+from colorama import init
+from termcolor import colored, cprint
 from colorama import Fore, Back, Style
-colorama.init()
 
-# Set the color semi-permanently
-print(Fore.CYAN)
-print("Text will continue to be cyan")
-print("until it is reset or changed")
-print(Style.RESET_ALL)
+# use Colorama to make Termcolor work on Windows too
+init()
 
-# Colorize a single line and then reset
-print(Fore.RED + 'You can colorize a single line.' + Style.RESET_ALL)
+# then use Termcolor for all colored text output
+print(colored('Hello, World!', 'green', 'on_red'))
 
-# Colorize a single word in the output
-print('Or a single ' + Back.GREEN + 'words' + Style.RESET_ALL + ' can be highlighted')
+prompt_prefix = colored("myco", "red") + "@"
 
-# Combine foreground and background color
-print(Fore.BLUE + Back.WHITE)
-print('Foreground, background, and styles can be combined')
-print("==========            ")
 
-print(Style.RESET_ALL)
-print('If unsure, reset everything back to normal.')
+class stdout:
+
+    @staticmethod
+    def write(text):
+        print("this?", text)
+
+
+def make_prompt(path, project = False):
+    if project:
+        return prompt_prefix + colored(path, "green") + ">"
+    return prompt_prefix + colored(path, "cyan") + ">"
+
 def is_project(path):
     x = path.glob(".myco")
     x = list(x)
@@ -36,25 +108,31 @@ def is_project(path):
         return x[0].is_file()
     else:
         return False
+
+
+
+
 class NavigationCMD(cmd.Cmd):
-    intro = "Welcome to MontyCarlo! Type help or ? to list the available commands ^.^ \n"
+    use_rawinput = 0
+
+    intro = colored("Welcome to MontyCarlo! Type help or ? to list the available commands ^.^ \n", "yellow")
     file = None
     project = None
     path = Path(sys.argv[0]).parent
-    prompt = f"{Fore.RED}myco{Style.RESET_ALL}@{Fore.CYAN}{path}{Style.RESET_ALL}> "
+    prompt = make_prompt(path)
 
     def do_cd(self, args):
         
 
         if args in ["..", ""]:
             NavigationCMD.path = NavigationCMD.path.parent
-            NavigationCMD.prompt = f"{Fore.RED}myco{Style.RESET_ALL}@{Fore.CYAN}{NavigationCMD.path}{Style.RESET_ALL}> "
+            NavigationCMD.prompt = make_prompt(NavigationCMD.path)
             return 
 
         new_path = NavigationCMD.path/args
         if new_path.is_dir():
             NavigationCMD.path = NavigationCMD.path/args
-            NavigationCMD.prompt = f"{Fore.RED}myco{Style.RESET_ALL}@{Fore.CYAN}{NavigationCMD.path}{Style.RESET_ALL}> "
+            NavigationCMD.prompt = make_prompt(NavigationCMD.path)
         else:
             print("Error: NOT A PATH")
 
@@ -94,11 +172,22 @@ class NavigationCMD(cmd.Cmd):
 
 class MontyCarloShell(NavigationCMD):
 
+    
+    def do_install(self, args):
+        print(colored("# Downloading databases !", "yellow"))
+        import MontyCarlo
+
+        pass
+
+    def do_uninstall(self, args):
+        print("# Uninstall is not yet available !")
+        pass
+
     def do_open(self, args):
         project_path = NavigationCMD.path/args
         if project_path.is_dir():
             if is_project(project_path):
-                MontyCarloProjectShell().cmdloop(0, 0)
+                MontyCarloProjectShell().cmdloop(project_path)
             else:
                 print("Not a project !")
         else:
@@ -121,7 +210,17 @@ class MontyCarloShell(NavigationCMD):
         
         root_folder = NavigationCMD.path/args
         root_folder.mkdir(parents=True, exist_ok=True)
+
+
+        # DIRECTORIES
+        (root_folder/'mat').mkdir(parents=True, exist_ok=True)
+        (root_folder/'geo').mkdir(parents=True, exist_ok=True)
+        (root_folder/'out').mkdir(parents=True, exist_ok=True)
+
+        # FILES
         (root_folder/'.myco').touch()
+        (root_folder/'main.py').touch()
+        (root_folder/'main.py').write_text(BOILER_PLATE)
 
 
 
@@ -129,8 +228,6 @@ class MontyCarloShell(NavigationCMD):
         print("\n")
 
 
-    def do_run():
-        pass
 
     def do_exit(self, arg):
         return True
@@ -141,14 +238,29 @@ class MontyCarloProjectShell(cmd.Cmd):
     intro = "Your project is now open! \n"
     path = Path(sys.argv[0]).parent
 
-
+    use_rawinput = 0
     def do_exit(self, args):
-        MontyCarloShell().cmdloop()
+        return True
 
 
     def cmdloop(self, path):
+        print(colored("Initing MontyCarlo, just a sec....", "yellow"))
+
+        import MontyCarlo
         MontyCarloProjectShell.path = path
-        MontyCarloProjectShell.prompt = f"{Fore.RED}myco{Style.RESET_ALL}@{Fore.GREEN}YourProject{Style.RESET_ALL}> "
+        MontyCarloProjectShell.prompt = make_prompt(path.name, project = True)
         super(MontyCarloProjectShell, self).cmdloop()
+
+    def do_run(self, args):
+        code = (MontyCarloProjectShell.path/'main.py').read_text()
+
+        class NAMESPACE:
+            try:
+                exec(code)
+            except:
+                print("smthin bad happened")
+        
+        del NAMESPACE
+        
 
 MontyCarloShell().cmdloop()
