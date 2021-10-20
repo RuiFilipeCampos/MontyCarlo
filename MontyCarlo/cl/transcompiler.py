5de43f43e001c76f1e598035f65cbcc5597c2138
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-10-20 09:38:43 +0100

    trying to figure this stuff out

--- MontyCarlo/cl/transcompiler.py
+++ MontyCarlo/cl/transcompiler.py
@@ -6,11 +6,95 @@ asdas adasd
 asd sad  # asdasd
 
 
-<Sphere>
+<Sphere  xxx >
 
 
 
 """
+
+
+
+
+class Token:
+    def parse_symbol(self):
+        pass
+
+class Tag:
+    def __init__(self):
+        self.getting_name = True
+        self.self_closing = None
+
+        self.end = False
+    
+
+    def parse_symbol(self, symbol):
+        if self.end:
+            self.next = self.determine_next_token()
+        
+        if symbol == "/":
+            self_closing = True
+            return False
+
+        if symbol == ">":
+            self.end = True
+            return False
+        
+        if self.getting_name:
+
+            if symbol == " ":
+                self.getting_name = False
+                return False
+
+            self.name += symbol
+
+        if symbol == " ":
+            return False
+
+        
+        self.getting_var_name = True
+
+        if self.getting_var_name:
+            
+
+        if self.getting_value:
+            pass
+
+        
+            
+        
+
+
+class PythonCode:
+    def __init__(self):
+        self.code = ""
+
+
+    def __bool__(self):
+        return True
+    
+    def parse_symbol(self, symbol):
+        if symbol == "<":
+            self.next = Tag()
+            return True
+
+        self.code += symbol
+        return False
+
+
+class SelfClosing:
+    pass
+
+
+
+def parse(code):
+
+    tokens = []
+    current_token = PythonCode()
+
+    for letter in code:
+        if current_token.parse_symbol(letter):
+            current_token = current_token.next
+
 
 
 def to_python(code):
