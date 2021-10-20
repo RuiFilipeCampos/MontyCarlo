1f8d63389a10c6879cf9a675f65c6202ee549029
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-10-20 10:47:21 +0100

    yee, pretty much got it

--- MontyCarlo/cl/transcompiler.py
+++ MontyCarlo/cl/transcompiler.py
@@ -14,50 +14,116 @@ asd sad  # asdasd
 
 
 
+class Variable:
+    def __init__(self, symbol):
+        self.name = symbol
+        self.value = ""
 
-class Token:
-    def parse_symbol(self):
-        pass
 
+
+DETERMINING_CLOSURE = 0
+GETTING_NAME = 1
+SEARCHING = 2
+GETTING_VAR_NAME = 3
+GETTING_VAR_VALUE = 4
+
+ENDING_TOKEN = -1
 class Tag:
-    def __init__(self):
-        self.getting_name = True
-        self.self_closing = None
 
-        self.end = False
+
+    def __init__(self):
+
+        self.state = DETERMINING_CLOSURE
+        self.vars = []
+
+        self.name = ""
+
+
+        self.type = ""
+
+    def __repr__(self):
+        if self.type == "self-closing":
+            return f"<{self.name} />"
+        if self.type == "start":
+            return f"<{self.name}>"
+        if self.type == "end":
+            return f"</{self.name}>"
+        
+        return f"<{self.name}>"
+
+
+    def determine_next_token(self, symbol):
+        if symbol == "<":
+            return Tag()
     
+        return PythonCode(symbol)
 
     def parse_symbol(self, symbol):
-        if self.end:
-            self.next = self.determine_next_token()
+
+        if self.state == ENDING_TOKEN:
+            self.next = self.determine_next_token(symbol)
+            return True
+
+        if self.state == DETERMINING_CLOSURE:
+            if symbol == "/":
+                self.type = "end"
+                self.getting_name = True
+            else:
+                self.type = "start"
+                self.name += symbol
+
+            self.state = GETTING_NAME
+            return False
         
+
+        # CHECK FOR END
         if symbol == "/":
-            self_closing = True
+            self.type = "self-closing"
             return False
 
         if symbol == ">":
-            self.end = True
+            self.state = ENDING_TOKEN
             return False
         
-        if self.getting_name:
+        if self.state == GETTING_NAME:
 
             if symbol == " ":
-                self.getting_name = False
+                self.state = SEARCHING
                 return False
 
             self.name += symbol
+            return False
 
-        if symbol == " ":
+        if self.state == SEARCHING:
+            if symbol == " ":
+                return False
+            
+            self.state = GETTING_VAR_NAME
+            self.current_variable = Variable(symbol)
             return False
 
         
-        self.getting_var_name = True
 
-        if self.getting_var_name:
+        if self.state == GETTING_VAR_NAME:
+            if symbol == " ":
+                raise RuntimeError("")
+
+            if symbol == "=":
+                self.state = GETTING_VAR_VALUE
+                return False
+            
+            self.current_variable.name += symbol
+            return False
             
 
-        if self.getting_value:
-            pass
+        if self.state == GETTING_VAR_VALUE:
+            if symbol == " ":
+                self.state = SEARCHING
+                self.vars.append(self.current_variable)
+                return False 
+
+            self.current_variable.value += symbol
+            return False
 
         
             
@@ -65,12 +131,15 @@ class Tag:
 
 
 class PythonCode:
-    def __init__(self):
-        self.code = ""
+    def __init__(self, symbol):
+        self.code = symbol
 
 
     def __bool__(self):
         return True
+
+    def __repr__(self):
+        return "<PythonCode>"
     
     def parse_symbol(self, symbol):
         if symbol == "<":
@@ -89,87 +158,61 @@ class SelfClosing:
 def parse(code):
 
     tokens = []
-    current_token = PythonCode()
+    current_token = PythonCode("")
+    tokens.append(current_token)
 
     for letter in code:
+        print(current_token, letter)
         if current_token.parse_symbol(letter):
+            
+
             current_token = current_token.next
-
-
-
-def to_python(code):
-
-    tokens = []
-    inside_tag = False
-
-    writing_name = False
-    writing_func_name = False
-
-    for letter in code:
-        if inside_tag:
-
-
-            if writing_func_name:
-                if letter == " ":
-                    writing_func_name = False
-                    continue
-
-                if letter == ">":
-                    writing_func_name = False
-                    tokens.append(">")
-                    continue
-
-                tokens[-1] += letter
-                continue
-
-
-
-            if letter == " ":
-                continue
-            
-            if letter == "=":
-                tokens.append("=")
-                tokens.append("")
-                continue
-
-            if letter == "/":
-                tokens.append("/")
-                continue
-
-            if letter == ">":
-                tokens.append(">")
-                inside_tag = False
-                continue
-
-
-
-            
-
-
-
-        if letter == "<":
-            tokens.append("<")
-            tokens.append("")
-            inside_tag = True
-            writing_func_name = True
-
-
+            tokens.append(current_token)
     return tokens
+
+
+# are these names even correct? xdd
+# gotta check
+
+def to_code(tag):
+
+
+def lexer(tokens):
+    code = ""
+    for token in tokens:
+        if isintance(token, PythonCode):
+            code += token.code
+            continue
+
+        if isinstance(token, Tag):
+            if token.type == "self-closing":
+                
 
 
 if __name__ == "__main__":
 
-    print(to_python(r"""
+    code = r"""
     def f(x):
         return x**2
     
+    <Hello/>
 
+    <World><HI/>
+    </World>
 
-    <Hello> <Hello>
     
     
     
-    """))
+    """
+
+    print("Input:")
+    print(code)
+    print("")
+
+    print("Output:")
+    print(parse(
+        code
+    ))
 
 
 
