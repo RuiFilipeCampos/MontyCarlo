1efb5d83ed98dd7dbd51a9962dcde03f94bd6a4b
RuiFilipeCampos <ruifilipedesousacampos@gmail.com>
2021-10-29 12:02:34 +0100

    need to add these to state
    since there will be 'external' transforms

--- MontyCarlo/geometry/csg/primitives/main.pyx
+++ MontyCarlo/geometry/csg/primitives/main.pyx
@@ -37,20 +37,20 @@ cdef class Primitive(CSGvol):
         )
 
 
-        translated = False
-        rotated = False
+        self.translated = False
+        self.rotated = False
 
         if kwargs['transforms']:
             for transformation in kwargs['transforms']:
 
                 if transformation[0] == "translate":
-                    translated = True
+                    self.translated = True
                     tmp_direct_transform[3]  += transformation[1]
                     tmp_direct_transform[7]  += transformation[2]
                     tmp_direct_transform[11] += transformation[3]
 
                 elif transformation[0] == "rotate":
-                    rotated = True
+                    self.rotated = True
                     axis = np.array([
                         transformation[1][0], 
                         transformation[1][1], 
@@ -138,5 +138,43 @@ def Translate(self, *args, **kwargs):
         (<Primitive> volume).inverse_transform[3] -= z
 
         Translate(*list(volume))
-            
+
+
+def Rotate(self, *args, axis = (0, 0, 1), angle=0):
+    axis = np.array([
+        transformation[1][0], 
+        transformation[1][1], 
+        transformation[1][2], 
+    ], 
+    dtype = np.float64 )
+
+    axis = axis/np.sqrt(np.sum(axis**2))
+
+    ux = axis[0]
+    uy = axis[1]
+    uz = axis[2]
+
+    angle = transformation[2]
+
+    rotation_matrix = np.zeros((4,4))
+
+    _cos = cos(angle)
+    oneMcos = 1 - _cos
+    _sin = sqrt(1 - _cos*_cos)
+
+    rotation_matrix[0, 0] = _cos + ux*ux*oneMcos
+    rotation_matrix[0, 1] = ux*uy*oneMcos - uz*_sin
+    rotation_matrix[0, 2] = ux*uz*oneMcos + uy*_sin
+
+    rotation_matrix[1, 0] = uy*ux*oneMcos + uz*_sin
+    rotation_matrix[1, 1] = _cos + uy*uy*oneMcos
+    rotation_matrix[1, 2] = uy*uz*oneMcos - ux*_sin
+
+    rotation_matrix[2, 0] = uz*ux*oneMcos-uy*_sin
+    rotation_matrix[2, 1] = uz*uy*oneMcos + ux*_sin
+    rotation_matrix[2, 2] = _cos + uz*uz*oneMcos
+
+    rotation_matrix[3, 3] = 1
+
+    tmp_direct_transform = rotation_matrix @ tmp_direct_transform
 
