ce3e6409d35d491a66f7ac1e57e538203b7c4f10
Rui Campos <ruifilipedesousacampos@gmail.com>
2022-01-29 09:54:01 +0000

    Fix attempt.

--- MontyCarlo/geometry/CSG.pyx
+++ MontyCarlo/geometry/CSG.pyx
@@ -356,7 +356,7 @@ cdef class CSGvol(BVH):
 
                 if (<V> self.ws[first.index]).main_intersect(state):
                     # Substitute the volume by its proxy
-                    self.ws[first.index] = <void*> (<CSGvol> self.ws[first.index]).get_proxy()
+                    self.ws[first.index] = <void*> ( (<CSGvol> self.ws[first.index]).get_proxy() )
 
                 first.distance  = (<V> self.ws[first.index])._get_safest_distance()
                 second.distance = INF
@@ -376,8 +376,6 @@ cdef class CSGvol(BVH):
 
                 if first.distance == INF:
                     if state.L < second.distance: # < first.distance
-
-                    
                         state.pos.x += state.dire.x*state.L
                         state.pos.y += state.dire.y*state.L
                         state.pos.z += state.dire.z*state.L
