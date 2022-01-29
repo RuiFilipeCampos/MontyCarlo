2f6b951d7a26494ed41818568ded570253bf38b1
Rui Campos <ruifilipedesousacampos@gmail.com>
2022-01-29 15:16:20 +0000

    Start django project.

--- server/config/settings.py
+++ server/config/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'engine',
     'geometry',
     'projects',
+    'materials',
 ]
 
 MIDDLEWARE = [
--- server/projects/models.py
+++ server/projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+
+class Project:
+    name = models.CharField(max_length=500)
